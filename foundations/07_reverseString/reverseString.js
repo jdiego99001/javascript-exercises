a7b7e31f97fa40453b11aee961420adb28725801
Juan Diego Garcia <jdiego99001@gmail.com>
2026-08-12 16:33:56 -0500

    ADDED CODE JS added code for answer the excercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = ""
 
+    let stringSplit = string.split("")
+
+    for (let i = (stringSplit.length - 1); i >= 0; i--) {
+        result = result + stringSplit[i]
+    }
+
+    return result
 };
 
 // Do not edit below this line
