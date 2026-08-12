a06e998be5a38d5bc1111decb8e6a0bc738bd83e
Juan Diego Garcia <jdiego99001@gmail.com>
2026-08-12 17:01:48 -0500

    ADD CODE JS added code to the function for answer the excercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for (let i = 0; i <= (nums.length - 1); i++) {
+        if (arr.includes(nums[i])) {
+            const indexElement = arr.indexOf(nums[i], 0)
+            arr.splice(indexElement, 1)
+        }
+        else {
+            continue;
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
